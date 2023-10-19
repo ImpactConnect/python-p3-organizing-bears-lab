52677c3888baaea124717ccdbeed750ada5a382f
Omotayo <kehindeomotayo11@gmail.com>
2023-10-19 17:24:30 +0100

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES 
+('Mr. Chocolate', 10, 'M', 'Brown', 'Friendly', 1),
+('Rowdy', 5, 'M', 'Black', 'Playful', 1),
+('Tabitha', 8, 'F', 'White', 'Calm', 1),
+('Sergeant Brown', 12, 'M', 'Brown', 'Grumpy', 1),
+('Melissa', 6, 'F', 'Brown', 'Energetic', 1),
+('Grinch', 15, 'M', 'Green', 'Mischevious', 1),
+('Wendy', 7, 'F', 'Brown', 'Sweet', 1),
+(NULL, NULL, NULL, NULL, NULL, NULL);
